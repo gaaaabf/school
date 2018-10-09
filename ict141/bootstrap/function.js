ba8814e74e203da99e415b9e1b1cb556500dd364
gaaaabf <43661643+gaaaabf@users.noreply.github.com>
2018-10-09 09:36:59 +0800

    Update function.js

--- ict141/bootstrap/function.js
+++ ict141/bootstrap/function.js
@@ -1,34 +1,4 @@
 var data = [];
-// function showAddForm() {
-// 	document.getElementById('pakita').innerHTML ='<form method="post" id="addForm">'+
-// 			'<h3>Add Data</h3>'+
-// 				'<div class="form-row align-items-center">'+
-// 					'<div class="col-sm-3 my-1">'+
-// 						'<label class="col-sm-2 col-form-label">First</label>'+
-// 						'<input type="text" class="form-control" id="ffirst" name="first" required>'+
-// 					'</div>'+
-// 					'<div class="col-sm-3 my-1">'+
-// 						'<label class="col-sm-2 col-form-label">Last</label>'+
-// 						'<input type="text" class="form-control" name="last" required>'+
-// 					'</div>'+
-// 				'</div>'+
-// 				'<div class="form-row align-items-center">'+
-// 					'<div class="col-sm-3 my-1">'+
-// 						'<label class="col-sm-2 col-form-label">Email</label>'+
-// 						'<input type="email" class="form-control" name="email" required>'+
-// 					'</div>'+
-// 					'<div class="col-sm-3 my-1">'+
-// 						'<label class="col-sm-2 col-form-label">Contact</label>'+
-// 						'<input type="text" class="form-control" name="contact" required>'+
-// 					'</div>'+
-// 				'</div>'+
-// 				'<br>'+
-// 				'<input type="submit" class="btn btn-primary" id="addBtn" value="Submit">'+
-// 			'</form>'
-// }
-
-
-
 function showAddForm() {
 	document.getElementById('update').style.display = "none" ;
 	document.getElementById('add').style.display = "block" ;
@@ -37,80 +7,81 @@ function showUpdateForm(count) {
 	document.getElementById('add').style.display = "none" ;
 	document.getElementById('update').style.display = "block" ;
 
-	document.getElementById('efirst').value = data[count-4];
-	document.getElementById('elast').value = data[count-3];
-	document.getElementById('eemail').value = data[count-2];
-	document.getElementById('econtact').value = data[count-1];
+	document.getElementById('efirst').value = data[count][0];
+	document.getElementById('elast').value = data[count][1];
+	document.getElementById('eemail').value = data[count][2];
+	document.getElementById('econtact').value = data[count][3];
 
-	document.getElementById('hidden').value = data[count];
-
+	document.getElementById('hidden').value = count;
 	}
 
-// function showUpdateForm(count) {
-// 	console.log(data[count]);
-// 	document.getElementById('pakita').innerHTML = '<form method="GET" id="updateForm">'+
-// 			'<h3><br>Edit Data</h3>'+
-// 			'<div class="form-row align-items-center">'+
-// 				'<div class="col-sm-3 my-1">'+
-// 					'<label class="col-sm-2 col-form-label">First</label>'+
-// 					'<input type="text" class="form-control" value="'+data[count-4]+'" name="firste">'+
-// 				'</div>'+
-// 				'<div class="col-sm-3 my-1">'+
-// 					'<label class="col-sm-2 col-form-label">Last</label>'+
-// 					'<input type="text" class="form-control" value="'+data[count-3]+'" name="laste">'+
-// 				'</div>'+
-// 			'</div>'+
-// 			'<div class="form-row align-items-center">'+
-// 				'<div class="col-sm-3 my-1">'+
-// 					'<label class="col-sm-2 col-form-label">Email</label>'+
-// 					'<input type="email" class="form-control" value="'+data[count-2]+'" name="emaile">'+
-// 				'</div>'+
-// 				'<div class="col-sm-3 my-1">'+
-// 					'<label class="col-sm-2 col-form-label">Contact</label>'+
-// 					'<input type="text" class="form-control" value="'+data[count-1]+'" name="contacte">'+
-// 				'</div>'+
-// 			'</div>'+
-// 			'<br>'+
-// 			'<input type="button" class="btn btn-primary" id="editBtn" value="Submit">'+
-// 		'</form>'
-// 	// data.splice(count,4,'test','test');
-// }
+function showList() {
+	console.log(data);
+	var test = '';
+
+	if (data.length >= 1) 
+	{
+		for (var i = 0; i != data.length; i++) {
+			test += "<tr><td>"+data[i][0]+"</td><td>"+data[i][1]+"</td><td>"+data[i][2]
+			+"</td><td>"+data[i][3]+"</td><td><button class='btn btn-primary' onclick='showUpdateForm("+data[i][4]+")'>Edit</button> <button class='btn btn-warning' onclick='deleteAction("+data[i][4]+")'>Delete</button></td></tr>";
+		}
+			document.getElementById('tbody').innerHTML = test;
+	}
+	else
+	{
+			document.getElementById('tbody').innerHTML = '<h1>NO DATA FOUND</h1>';
+	}
+}
+
+function deleteAction(count) {
+	data.splice(count,1)
+	console.log(data);
+	showList();
+}
+
 
 $(document).ready(function() {
 	var count = 0;
 
 	$("#addBtn").click(function(){
-
+		console.log(data);
 		var fname = $('input[name=first]').val();
 		var lname = $('input[name=last]').val();
 		var email = $('input[name=email]').val();
 		var contact = $('input[name=contact]').val();
 
-		data.push(fname,lname,email,contact);
-		count+=4;
+			// if ( fname == '' || lname == '' || email == '' || contact == '') 
+			// {
+			// 	alert("Please input value");
+				
+			// }
+				data.push([fname,lname,email,contact,count]);
+				count++;
 
-
-        $('tbody').append("<tr><td>"+fname+"</td><td>"+lname+"</td><td>"+email
-+"</td><td>"+contact+"</td><td><button class='btn btn-primary' onclick='showUpdateForm("+count+")'>Edit</button> <button class='btn btn-warning' onclick='showUpdateForm("+count+")'>Delete</button></td></tr>");
-		$("#addForm").trigger("reset");      
-		// $("#addForm").hide();            });
-		// $("#updateBtn").click(function(){     // 
+					showList();
+					$("#addForm").trigger("reset");
+//         $('tbody').append("<tr><td>"+fname+"</td><td>"+lname+"</td><td>"+email
+// +"</td><td>"+contact+"</td><td><button class='btn btn-primary' onclick='showUpdateForm("+count+")'>Edit</button> <button class='btn btn-warning' onclick='showUpdateForm("+count+")'>Delete</button></td></tr>");
+// 		$("#addForm").trigger("reset");      
+// 		$("#addForm").hide();       
 		});
 
 	$("#editBtn").click(function(){
+
 
 		var fname = $('input[name=firste]').val();
 		var lname = $('input[name=laste]').val();
 		var email = $('input[name=emaile]').val();
 		var contact = $('input[name=contacte]').val();
 		var count = $('input[name=hidden]').val();
-		var tsk = $(this).data('row');
-			console.log(tsk);
-		data.splice(count,4,fname,lname,email,contact);
 
-        $('tbody').append("<tr><td>"+fname+"</td><td>"+lname+"</td><td>"+email
-+"</td><td>"+contact+"</td><td><button class='btn btn-primary' onclick='showUpdateForm("+count+")'>Edit</button></td></tr>");
-		$("#addForm").trigger("reset");      
+		console.log(count);
+
+		data[count].splice(0,5,fname,lname,email,contact);
+
+		showList();
+
+		$("#updateForm").trigger("reset");      
 		// $("#addForm").hide();            });
 		// $("#updateBtn").click(function(){     // 
 		});
